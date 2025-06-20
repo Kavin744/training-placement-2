6157ee73b9c18b99cd70f7eacf91c966d84dc0c9
Kavin Kumar <147464206+Kavin744@users.noreply.github.com>
2025-06-20 10:00:26 +0530

    Create bubble sort.java

---
+++ 20-06-25/bubble sort.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
